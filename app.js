03b29f3ab08ba10fb903e4b5423e91797146d831
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-01-27 16:08:09 +0300

    styling

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
 const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
-const imgUrl = `http://image.tmdb.org/t/p/w200/`;
+const imgUrl = `http://image.tmdb.org/t/p/w300/`;
 
 document.querySelector(".form").addEventListener("submit", event => {
   event.preventDefault();
